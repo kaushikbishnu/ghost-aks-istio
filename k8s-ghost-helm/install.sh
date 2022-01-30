c55e06bf51c55b19b250f5dd1b5a9a962d363993
Kaushik Bishnu <kaushik.bishnu@gmail.com>
2022-01-30 06:52:21 +0100

    adding pipeline file

--- k8s-ghost-helm/install.sh
+++ k8s-ghost-helm/install.sh
@@ -1,10 +1,11 @@
-helm upgrade --install ghost-nordcloud ./ghost-nordcloud/
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 kubectl create namespace istio-system
 helm install istio-base istio/base -n istio-system
-helm install istiod istio/istiod -n istio-system --wait
+helm install istiod istio/istiod -n istio-system #--wait
 kubectl create namespace istio-ingress
 kubectl label namespace istio-ingress istio-injection=enabled
-helm install istio-ingress istio/gateway -n istio-ingress --wait
+kubectl label namespace default istio-injection=enabled
+helm install istio-ingress istio/gateway -n istio-ingress #--wait
 helm status istiod -n istio-system
+helm upgrade --install ghost-nordcloud ./ghost-nordcloud/ #-n istio-ingress
