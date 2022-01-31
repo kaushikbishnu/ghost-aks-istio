0998a697f9f3b9dc101d4f92417fd7e25ca9fb1f
Kaushik Bishnu <kaushik.bishnu@gmail.com>
2022-02-01 00:06:19 +0100

    updating path of ghost helm file

--- k8s-ghost-helm/scripts/ghost.sh
+++ k8s-ghost-helm/scripts/ghost.sh
@@ -1,6 +1,6 @@
 echo "---------INSTALLING GHOST---------"
 
-helm upgrade --install ghost-nordcloud ./ghost-nordcloud/ #-n istio-ingress
+helm upgrade --install ghost-nordcloud ../ghost-nordcloud/ #-n istio-ingress
 
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export INGRESS_DOMAIN=${INGRESS_HOST}
