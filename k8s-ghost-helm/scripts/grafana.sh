76ca4b88d39faa8e8acfcab14aed21c2d2e82daa
Kaushik Bishnu <kaushik.bishnu@gmail.com>
2022-02-03 15:56:17 +0100

    monitoring tools deployment

--- k8s-ghost-helm/scripts/grafana.sh
+++ k8s-ghost-helm/scripts/grafana.sh
@@ -1,5 +1,5 @@
-echo "---------INSTALLING prometheus---------"
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/prometheus.yaml
+echo "---------INSTALLING grafana---------"
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/grafana.yaml
 
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 export INGRESS_DOMAIN=${INGRESS_HOST}
@@ -9,48 +9,45 @@ cat <<EOF | kubectl apply -f -
 apiVersion: networking.istio.io/v1alpha3
 kind: Gateway
 metadata:
-  name: prometheus-gateway
+  name: garfana-server-gateway
   namespace: istio-system
 spec:
   selector:
     istio: ingressgateway
   servers:
   - port:
-      number: 80
-      name: http-prom
-      protocol: HTTP
+      number: 15021
+      name: http-grafana
+      protocol: HTTP    
     hosts:
     - "${INGRESS_DOMAIN}"
 ---
 apiVersion: networking.istio.io/v1alpha3
 kind: VirtualService
 metadata:
-  name: prometheus-vs
+  name: garfana-server
   namespace: istio-system
 spec:
   hosts:
   - "${INGRESS_DOMAIN}"
   gateways:
-  - prometheus-gateway
+  - garfana-server-gateway
   http:
   - match:
-    - uri:
-        prefix: /prometheus
     route:
     - destination:
-        host: prometheus
+        host: grafana
         port:
-          number: 9090
+          number: 3000
 ---
 apiVersion: networking.istio.io/v1alpha3
 kind: DestinationRule
 metadata:
-  name: prometheus
+  name: grafana
   namespace: istio-system
 spec:
-  host: prometheus
+  host: grafana
   trafficPolicy:
     tls:
       mode: DISABLE
----
 EOF
