a1893263d156c0f3642226a1719bbff9c6f1829c
Kaushik Bishnu <kaushik.bishnu@gmail.com>
2022-02-17 19:17:32 +0100

    chnaged the app_name parameters

--- k8s-ghost-helm/scripts/az.kubectl.sh
+++ k8s-ghost-helm/scripts/az.kubectl.sh
@@ -1,4 +1,4 @@
 az login --service-principal -u 93031176-d2d8-4035-b9ef-00b5a4febee6 -p U.4p51.gLtiR10Yemwzd5weld9-Z6X5Blk --tenant bad3cd3d-a88a-478d-a251-43f7355aad77
 az account set --subscription 1bd78ea7-13c0-4d71-bc70-b6a822ea4a46
-az aks get-credentials --resource-group rg-nordcloud-ghost --name ghost-k8s-cluster
+az aks get-credentials --resource-group rg-nordcloud-ghost --name nordcloud-test-k8s-cluster
 
